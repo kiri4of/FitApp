8af20c6d7fe85816021c1da3bd9f59b71c589db8
Kiri4of <kopak7890@gmail.com>
2025-05-21 16:11:53 +0100

    Initial Commit

---
+++ FitApp/FitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitAppApp.swift
+//  FitApp
+//
+//  Created by Kiri4of on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
